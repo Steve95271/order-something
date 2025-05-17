e8e57bfeffe062315d9c2c6f1683c843bb8b66ec
Yingcong Feng <FengY52@cardiff.ac.uk>
2025-05-17 15:00:52 +0100

    project set up

---
+++ holy-grail/src/main/java/com/order_something/holy_grail/HolyGrailApplication.java
@@ -0,0 +1,13 @@
+package com.order_something.holy_grail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolyGrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolyGrailApplication.class, args);
+	}
+
+}
---
+++ holy-grail/src/test/java/com/order_something/holy_grail/HolyGrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.order_something.holy_grail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolyGrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
