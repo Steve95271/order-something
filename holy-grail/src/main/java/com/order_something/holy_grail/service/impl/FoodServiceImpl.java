59a2e98a851f84f242dd126dfc8517e6f01c2a9f
Yingcong Feng <FengY52@cardiff.ac.uk>
2025-05-19 08:45:58 +0100

    created FoodMapper, FoorRepository and FoodServiceImpl java files

--- holy-grail/src/main/java/com/order_something/holy_grail/service/impl/FoodServiceImpl.java
+++ holy-grail/src/main/java/com/order_something/holy_grail/service/impl/FoodServiceImpl.java
@@ -1,4 +1,8 @@
 package com.order_something.holy_grail.service.impl;
 
-public class FoodServiceImpl {
+import com.order_something.holy_grail.service.FoodService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FoodServiceImpl implements FoodService {
 }
